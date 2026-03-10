828d5166bb654007dca899896cf0cc7e8192f380
MOHANIOZ <kmohankmohan462@gmail.com>
2026-03-10 17:11:15 +0530

    Fixed security v1 path

--- src/main/java/com/bankApp/banking_app/controller/AccountController.java
+++ src/main/java/com/bankApp/banking_app/controller/AccountController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/accounts")
+@RequestMapping("/api/v1/accounts")
 public class AccountController {
 
     private final AccountService accountService;
